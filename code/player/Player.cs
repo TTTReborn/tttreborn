2278d644000b2460a2b77d9f19114a742cb45d4c
Alf Müller <4lf-mueller@gmx.de>
2021-06-13 15:44:39 +0200

    Initial InspectMenu and fixed continous inspectmenu stream

--- code/player/Player.cs
+++ code/player/Player.cs
@@ -17,8 +17,7 @@ namespace TTTReborn.Player
         private static int WeaponDropVelocity { get; set; } = 300;
 
         public BaseRole Role {
-            set
-            {
+            set {
                 role = value;
 
                 if (IsServer)
@@ -40,6 +39,8 @@ namespace TTTReborn.Player
         private float inspectCorpseDistance = 80f;
 
         private TimeSince timeSinceDropped = 0;
+
+        private bool isInspecting = false;
 
         public TTTPlayer()
         {
@@ -142,12 +143,15 @@ namespace TTTReborn.Player
             }
 
             TickPlayerUse();
-            TickAttemptInspectPlayerCorpse();
+
             TickPlayerDropWeapon();
 
             SimulateActiveChild(client, ActiveChild);
 
-            TickAttemptInspectPlayerCorpse();
+            if (IsServer)
+            {
+                TickAttemptInspectPlayerCorpse();
+            }
 
             PawnController controller = GetActiveController();
             controller?.Simulate(client, this, GetActiveAnimator());
@@ -189,30 +193,39 @@ namespace TTTReborn.Player
 
         private void TickAttemptInspectPlayerCorpse()
         {
-            if (IsServer)
+            using (Prediction.Off())
             {
-                using (Prediction.Off())
+                To client = To.Single(this);
+                PlayerCorpse playerCorpse = IsLookingAtPlayerCorpse();
+
+                if (playerCorpse != null)
                 {
-                    To client = To.Single(this);
-                    PlayerCorpse playerCorpse = IsLookingAtPlayerCorpse();
-
-                    if (playerCorpse != null)
+                    if (!isInspecting)
                     {
-                        if (Input.Down(InputButton.Use) && !playerCorpse.IsIdentified)
-                        {
-                            playerCorpse.IsIdentified = true;
-
-                            ClientConfirmPlayer(this, playerCorpse.Player, playerCorpse.Player.Role.Name);
-                        }
+                        isInspecting = true;
 
                         // Send the request to the player looking at the player corpse.
                         // https://wiki.facepunch.com/sbox/RPCs#targetingplayers
                         ClientOpenInspectMenu(client, playerCorpse.Player, playerCorpse.IsIdentified);
-
-                        return;
                     }
 
+                    if (!playerCorpse.IsIdentified && Input.Down(InputButton.Use))
+                    {
+                        playerCorpse.IsIdentified = true;
+
+                        ClientConfirmPlayer(this, playerCorpse.Player, playerCorpse.Player.Role.Name);
+
+                        ClientOpenInspectMenu(client, playerCorpse.Player, playerCorpse.IsIdentified);
+                    }
+
+                    return;
+                }
+
+                if (isInspecting)
+                {
                     ClientCloseInspectMenu(client);
+
+                    isInspecting = false;
                 }
             }
         }
@@ -242,14 +255,17 @@ namespace TTTReborn.Player
                 info.Damage *= 2.0f;
             }
 
-            if (info.Attacker is TTTPlayer attacker && attacker != this)
+            using (Prediction.Off())
             {
-                attacker.DidDamage(info.Position, info.Damage, ((float)Health).LerpInverse(100, 0));
-            }
+                if (info.Attacker is TTTPlayer attacker && attacker != this)
+                {
+                    attacker.DidDamage(info.Position, info.Damage, ((float) Health).LerpInverse(100, 0));
+                }
 
-            if (info.Weapon != null)
-            {
-                TookDamage(info.Weapon.IsValid() ? info.Weapon.Position : info.Attacker.Position);
+                if (info.Weapon != null)
+                {
+                    TookDamage(info.Weapon.IsValid() ? info.Weapon.Position : info.Attacker.Position);
+                }
             }
 
             // Play pain sounds
--- code/ui/InspectMenu.cs
+++ code/ui/InspectMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
 
@@ -21,33 +22,160 @@ namespace TTTReborn.UI
 
         private bool isShowing = false;
 
-        private Label InspectLabel { set; get; }
+        private Header header { set; get; }
+
+        private Content content { set; get; }
+
+        private Footer footer { set; get; }
+
+        private ConfirmationPanel confirmationPanel { get; set; }
 
         public InspectMenu()
         {
             Instance = this;
+            IsShowing = false;
+
             StyleSheet.Load("/ui/InspectMenu.scss");
 
-            InspectLabel = Add.Label("");
+            header = new Header(this);
+            header.AddClass("hide");
 
-            IsShowing = false;
+            content = new Content(this);
+            content.AddClass("hide");
+
+            footer = new Footer(this);
+            content.AddClass("hide");
+
+            confirmationPanel = new ConfirmationPanel(this);
         }
 
         public void InspectCorpse(TTTPlayer deadPlayer, bool isIdentified)
         {
             IsShowing = true;
 
-            // TODO: Setup proper hud, for now everything is just being thrown into "InspectLabel"
             if (isIdentified)
             {
-                InspectLabel.Text = $"{deadPlayer.GetClientOwner()?.Name}\n"
-                    + $"{deadPlayer.Role.Name}";
+                confirmationPanel.SetClass("hide", true);
+
+                header.SetPlayer(deadPlayer);
+                header.SetClass("hide", false);
+
+                content.SetPlayer(deadPlayer);
+                content.SetClass("hide", false);
+
+                footer.SetClass("hide", false);
             }
             else
             {
-                InspectLabel.Text = "Press E to identify this body.";
+                header.SetClass("hide", true);
+                content.SetClass("hide", true);
+                footer.SetClass("hide", true);
+
+                confirmationPanel.SetClass("hide", false);
+            }
+        }
+
+        private class Header : Panel
+        {
+            public Label PlayerLabel { get; set; }
+
+            public Label RoleLabel { get; set; }
+
+            public Header(Panel parent)
+            {
+                Parent = parent;
+
+                PlayerLabel = Add.Label("", "player");
+                RoleLabel = Add.Label("", "role");
+            }
+
+            public void SetPlayer(TTTPlayer player)
+            {
+                PlayerLabel.Text = player.GetClientOwner().Name;
+
+                RoleLabel.Text = player.Role.Name;
+                RoleLabel.Style.FontColor = player.Role.Color;
+            }
+        }
+
+        private class Content : Panel
+        {
+            public ImageWrapper PlayerImage { get; set; }
+
+            public List<InspectItem> inspectItems { get; set; } = new();
+
+            public Content(Panel parent)
+            {
+                Parent = parent;
+
+                PlayerImage = new ImageWrapper(this);
+                PlayerImage.AddClass("playericon");
+
+                InspectItem inspectWeapon = new InspectItem(this);
+                inspectWeapon.ImageWrapper.SetImageSource("");
+                inspectWeapon.InspectItemLabel.Text = "Pistol";
+
+                inspectItems.Add(inspectWeapon);
+            }
+
+            public void SetPlayer(TTTPlayer player)
+            {
+                PlayerImage.SetImageSource(""); // TODO fetch player's steam avatar
+            }
+        }
+
+        private class ImageWrapper : Panel
+        {
+            public Image Image { get; set; }
+
+            public ImageWrapper(Panel parent)
+            {
+                Parent = parent;
+            }
+
+            public void SetImageSource(string source)
+            {
+                Image = Add.Image(source);
+            }
+        }
+
+        private class InspectItem : Panel
+        {
+            public ImageWrapper ImageWrapper { get; set; }
+
+            public Label InspectItemLabel { get; set; }
+
+            public InspectItem(Panel parent)
+            {
+                Parent = parent;
+
+                ImageWrapper = new ImageWrapper(this);
+                InspectItemLabel = Add.Label("", "inspectItemName");
+            }
+        }
+
+        private class Footer : Panel
+        {
+            public Label FooterLabel { get; set; }
+
+            public Footer(Panel parent)
+            {
+                Parent = parent;
+
+                FooterLabel = Add.Label("x confirmed this body", "inspect");
+            }
+        }
+
+        private class ConfirmationPanel : Panel
+        {
+            public Label InspectLabel { get; set; }
+
+            public ConfirmationPanel(Panel parent)
+            {
+                Parent = parent;
+
+                InspectLabel = Add.Label("Press E to confirm", "inspect");
             }
         }
     }
-
 }
