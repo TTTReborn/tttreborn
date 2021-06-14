062a5ddca49e861c2aa7462e54a32f9a28ff88cc
Said Adibi <saidadibi4@gmail.com>
2021-06-14 17:57:48 +0200

    Set role colors to proper color

--- code/roles/InnocentRole.cs
+++ code/roles/InnocentRole.cs
@@ -3,7 +3,7 @@ namespace TTTReborn.Roles
     [RoleAttribute("Innocent")]
     public class InnocentRole : BaseRole
     {
-        public override Color Color => Color.Green;
+        public override Color Color => Color.FromBytes(27, 197, 78);
 
         public InnocentRole() : base()
         {
--- code/roles/TraitorRole.cs
+++ code/roles/TraitorRole.cs
@@ -3,7 +3,7 @@ namespace TTTReborn.Roles
     [RoleAttribute("Traitor")]
     public class TraitorRole : BaseRole
     {
-        public override Color Color => Color.Red;
+        public override Color Color => Color.FromBytes(223, 41, 53);
 
         public TraitorRole() : base()
         {
