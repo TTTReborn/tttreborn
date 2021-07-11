e70afe7ec488cc2c3d6508d75fc2f74167496059
Alf21 <alf21@neoxult.de>
2021-07-11 04:07:22 +0200

    Shop does not require created object anymore (just for initialization) (#108)

--- code/items/IBuyableItem.cs
+++ code/items/IBuyableItem.cs
@@ -1,12 +1,55 @@
+using System;
+
 using TTTReborn.Player;
 
 namespace TTTReborn.Items
 {
+    public struct ShopItemData
+    {
+        public string Name;
+        public string Description;
+        public int Price;
+        public Type Type;
+
+        public bool IsBuyable(TTTPlayer player)
+        {
+            if (Type.IsSubclassOf(typeof(TTTEquipment)))
+            {
+                return !(player.Inventory as Inventory).IsCarryingType(Type);
+            }
+            else if (Type.IsSubclassOf(typeof(TTTPerk)))
+            {
+                return !(player.Inventory as Inventory).Perks.Has(Name);
+            }
+            else if (Type.IsSubclassOf(typeof(TTTWeapon)))
+            {
+                return !(player.Inventory as Inventory).IsCarryingType(Type);
+            }
+
+            return false;
+        }
+    }
+
     public interface IBuyableItem : IItem
     {
         int Price { get; }
 
-        bool IsBuyable(TTTPlayer player);
+        ShopItemData CreateItemData()
+        {
+            ShopItemData itemData = new ShopItemData
+            {
+                Name = Name,
+                Price = Price,
+                Type = GetType()
+            };
+
+            if (this is ICarriableItem carriableItem)
+            {
+                itemData.Description = $"Slot: {(int) carriableItem.HoldType}";
+            }
+
+            return itemData;
+        }
 
         void OnPurchase(TTTPlayer player)
         {
