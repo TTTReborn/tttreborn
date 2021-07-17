77fad0d2ad58c0dd912effae23a63e1d03d5ee8c
Matthew Zegar <matthewzegar@gmail.com>
2021-07-16 21:45:20 -0700

    Add freecam and third person camera

---
+++ code/player/camera/FreeSpectateCamera.cs
@@ -0,0 +1,70 @@
+﻿using Sandbox;
+
+namespace TTTReborn.Player.Camera
+{
+    public partial class FreeSpectateCamera : Sandbox.Camera
+    {
+        private Angles _lookAngles;
+        private Vector3 _moveInput;
+
+        private Vector3 _targetPos;
+        private Rotation _targetRot;
+
+        private float _moveSpeed;
+
+        private const float LERP_MODE = 0;
+        private const int FIELD_OF_VIEW_OVERRIDE = 70;
+
+        public override void Activated()
+        {
+            base.Activated();
+
+            FieldOfView = FIELD_OF_VIEW_OVERRIDE;
+
+            _targetPos = CurrentView.Position;
+            _targetRot = CurrentView.Rotation;
+
+            Pos = _targetPos;
+            Rot = _targetRot;
+            _lookAngles = Rot.Angles();
+        }
+
+        public override void Update()
+        {
+            Client player = Local.Client;
+            if (player == null)
+            {
+                return;
+            }
+
+            Vector3 mv = _moveInput.Normal * 300 * RealTime.Delta * Rot * _moveSpeed;
+
+            _targetRot = Rotation.From(_lookAngles);
+            _targetPos += mv;
+
+            Pos = Vector3.Lerp(Pos, _targetPos, 10 * RealTime.Delta * (1 - LERP_MODE));
+            Rot = Rotation.Slerp(Rot, _targetRot, 10 * RealTime.Delta * (1 - LERP_MODE));
+        }
+
+        public override void BuildInput(InputBuilder input)
+        {
+            _moveInput = input.AnalogMove;
+
+            _moveSpeed = 1;
+            if (input.Down(InputButton.Run))
+            {
+                _moveSpeed = 5;
+            }
+
+            if (input.Down(InputButton.Duck))
+            {
+                _moveSpeed = 0.2f;
+            }
+
+            _lookAngles += input.AnalogLook * (FIELD_OF_VIEW_OVERRIDE / 80.0f);
+            _lookAngles.roll = 0;
+
+            base.BuildInput(input);
+        }
+    }
+}
