af3edbbb8175a123f9d9714eccade3c287f2cd02
Alf Müller <4lf-mueller@gmx.de>
2021-07-31 02:09:49 +0200

    Added left panel resizing support

--- code/ui/generalhud/menu/DraggableHeader.cs
+++ code/ui/generalhud/menu/DraggableHeader.cs
@@ -18,11 +18,13 @@ namespace TTTReborn.UI
 
         protected override void OnMouseDown(MousePanelEvent e)
         {
-            if (IsVisible)
+            if (!IsVisible)
             {
-                _draggingMouseStartPosition = MousePosition;
-                IsDragging = true;
+                return;
             }
+
+            _draggingMouseStartPosition = MousePosition;
+            IsDragging = true;
         }
 
         protected override void OnMouseUp(MousePanelEvent e)
@@ -32,47 +34,49 @@ namespace TTTReborn.UI
 
         protected override void OnMouseMove(MousePanelEvent e)
         {
-            if (IsDragging)
+            if (!IsDragging)
             {
-                float screenWidth = Screen.Width;
-                float screenHeight = Screen.Height;
-
-                Vector2 position = new Vector2(
-                    (MousePosition.x - _draggingMouseStartPosition.x) + Parent.ComputedStyle.Left.Value.GetPixels(screenWidth),
-                    (MousePosition.y - _draggingMouseStartPosition.y) + Parent.ComputedStyle.Top.Value.GetPixels(screenHeight)
-                );
-
-                float parentWidth = Parent.ComputedStyle.Width.Value.GetPixels(screenWidth);
-                float parentHeight = Parent.ComputedStyle.Height.Value.GetPixels(screenHeight);
-
-                if (position.x < 0f)
-                {
-                    Parent.Style.Left = Length.Pixels(0f);
-                }
-                else if (position.x + parentWidth > screenWidth)
-                {
-                    Parent.Style.Left = Length.Pixels(screenWidth - parentWidth);
-                }
-                else
-                {
-                    Parent.Style.Left = Length.Pixels(position.x);
-                }
-
-                if (position.y < 0f)
-                {
-                    Parent.Style.Top = Length.Pixels(0f);
-                }
-                else if (position.y + parentHeight > screenHeight)
-                {
-                    Parent.Style.Top = Length.Pixels(screenHeight - parentHeight);
-                }
-                else
-                {
-                    Parent.Style.Top = Length.Pixels(position.y);
-                }
-
-                Parent.Style.Dirty();
+                return;
             }
+
+            float screenWidth = Screen.Width;
+            float screenHeight = Screen.Height;
+
+            Vector2 position = new Vector2(
+                (MousePosition.x - _draggingMouseStartPosition.x) + Parent.ComputedStyle.Left.Value.GetPixels(screenWidth),
+                (MousePosition.y - _draggingMouseStartPosition.y) + Parent.ComputedStyle.Top.Value.GetPixels(screenHeight)
+            );
+
+            float parentWidth = Parent.ComputedStyle.Width.Value.GetPixels(screenWidth);
+            float parentHeight = Parent.ComputedStyle.Height.Value.GetPixels(screenHeight);
+
+            if (position.x < 0f)
+            {
+                Parent.Style.Left = Length.Pixels(0f);
+            }
+            else if (position.x + parentWidth > screenWidth)
+            {
+                Parent.Style.Left = Length.Pixels(screenWidth - parentWidth);
+            }
+            else
+            {
+                Parent.Style.Left = Length.Pixels(position.x);
+            }
+
+            if (position.y < 0f)
+            {
+                Parent.Style.Top = Length.Pixels(0f);
+            }
+            else if (position.y + parentHeight > screenHeight)
+            {
+                Parent.Style.Top = Length.Pixels(screenHeight - parentHeight);
+            }
+            else
+            {
+                Parent.Style.Top = Length.Pixels(position.y);
+            }
+
+            Parent.Style.Dirty();
         }
 
         private bool IsInScreen(Vector2 position)
--- code/ui/generalhud/menu/ResizeablePanel.cs
+++ code/ui/generalhud/menu/ResizeablePanel.cs
@@ -1,4 +1,4 @@
-using System.Numerics;
+using System;
 using Sandbox;
 using Sandbox.UI;
 
@@ -6,7 +6,51 @@ namespace TTTReborn.UI
 {
     public partial class ResizeablePanel : Panel
     {
+        public enum DragAnchor {
+            TOPLEFT,
+            TOP,
+            TOPRIGHT,
+            RIGHT,
+            BOTTOMRIGHT,
+            BOTTOM,
+            BOTTOMLEFT,
+            LEFT
+        }
+
         public bool IsDragging { get; private set; } = false;
+
+        public bool CanStartDragging
+        {
+            get
+            {
+                return _canStartDragging;
+            }
+            private set
+            {
+                _canStartDragging = value;
+
+                SetClass("draggable", _canStartDragging);
+            }
+        }
+        private bool _canStartDragging = false;
+
+        public DragAnchor? CurrentDragAnchor
+        {
+            get
+            {
+                return _currentDragAnchor;
+            }
+            private set
+            {
+                _currentDragAnchor = value;
+
+                CanStartDragging = _currentDragAnchor != null;
+            }
+        }
+
+        private DragAnchor? _currentDragAnchor = null;
+
+        public const float DRAG_OBLIGINGNESS = 8f;
 
         private Vector2 _draggingMouseStartPosition;
         private Vector2 _sizeBeforeDraggingStarted;
@@ -20,16 +64,18 @@ namespace TTTReborn.UI
         {
             base.OnMouseDown(e);
 
-            if (IsVisible)
+            if (!IsVisible || !CanStartDragging)
             {
-                _draggingMouseStartPosition = MousePosition;
-                _sizeBeforeDraggingStarted = new Vector2(
-                    ComputedStyle.Width.Value.GetPixels(Screen.Width),
-                    ComputedStyle.Height.Value.GetPixels(Screen.Height)
-                );
-
-                IsDragging = true;
+                return;
             }
+
+            _draggingMouseStartPosition = MousePosition;
+            _sizeBeforeDraggingStarted = new Vector2(
+                ComputedStyle.Width.Value.GetPixels(Screen.Width),
+                ComputedStyle.Height.Value.GetPixels(Screen.Height)
+            );
+
+            IsDragging = true;
         }
 
         protected override void OnMouseUp(MousePanelEvent e)
@@ -43,30 +89,103 @@ namespace TTTReborn.UI
         {
             base.OnMouseMove(e);
 
-            if (IsDragging)
+            if (!IsDragging)
             {
-                float screenWidth = Screen.Width;
-                float screenHeight = Screen.Height;
+                return;
+            }
 
-                Vector2 delta = new Vector2(
-                    (MousePosition.x - _draggingMouseStartPosition.x),
-                    (MousePosition.y - _draggingMouseStartPosition.y)
-                );
+            float screenWidth = Screen.Width;
+            float screenHeight = Screen.Height;
 
-                Vector2 position = new Vector2(
-                    ComputedStyle.Left.Value.GetPixels(screenWidth),
-                    ComputedStyle.Top.Value.GetPixels(screenHeight)
-                );
+            Vector2 delta = new Vector2(
+                (MousePosition.x - _draggingMouseStartPosition.x),
+                (MousePosition.y - _draggingMouseStartPosition.y)
+            );
 
-                Vector2 size = new Vector2(
-                    ComputedStyle.Width.Value.GetPixels(screenWidth),
-                    ComputedStyle.Height.Value.GetPixels(screenHeight)
-                );
+            Vector2 position = new Vector2(
+                ComputedStyle.Left.Value.GetPixels(screenWidth),
+                ComputedStyle.Top.Value.GetPixels(screenHeight)
+            );
 
-                Style.Width = Length.Pixels(_sizeBeforeDraggingStarted.x + delta.x);
-                Style.Height = Length.Pixels(_sizeBeforeDraggingStarted.y + delta.y);
+            Vector2 size = new Vector2(
+                ComputedStyle.Width.Value.GetPixels(screenWidth),
+                ComputedStyle.Height.Value.GetPixels(screenHeight)
+            );
 
-                Style.Dirty();
+            float width = size.x;
+            float height = size.y;
+            float x = position.x;
+            float y = position.y;
+
+            switch (CurrentDragAnchor)
+            {
+                case DragAnchor.LEFT:
+                    width = size.x - delta.x;
+                    x = position.x + delta.x;
+
+                    break;
+                case DragAnchor.RIGHT:
+                    width = _sizeBeforeDraggingStarted.x + delta.x;
+                    height = _sizeBeforeDraggingStarted.y + delta.y;
+
+                    break;
+            }
+
+            // TODO improve fast mouse dragging (set x to min left and width to max width etc.)
+            if (ComputedStyle.MinWidth != null && ComputedStyle.MinWidth.Value.GetPixels(screenWidth) > width)
+            {
+                return;
+            }
+
+            if (ComputedStyle.MaxWidth != null && ComputedStyle.MaxWidth.Value.GetPixels(screenWidth) < width)
+            {
+                return;
+            }
+
+            if (ComputedStyle.MinHeight != null && ComputedStyle.MinHeight.Value.GetPixels(screenHeight) > height)
+            {
+                return;
+            }
+
+            if (ComputedStyle.MaxHeight != null && ComputedStyle.MaxHeight.Value.GetPixels(screenHeight) < height)
+            {
+                return;
+            }
+
+            Style.Width = Length.Pixels(width);
+            Style.Height = Length.Pixels(height);
+            Style.Left = Length.Pixels(x);
+            Style.Top = Length.Pixels(y);
+            Style.Dirty();
+        }
+
+        public override void Tick()
+        {
+            if (!IsVisible || ComputedStyle == null || IsDragging)
+            {
+                return;
+            }
+
+            float screenWidth = Screen.Width;
+            float screenHeight = Screen.Height;
+
+            Vector2 size = new Vector2(
+                ComputedStyle.Width.Value.GetPixels(screenWidth),
+                ComputedStyle.Height.Value.GetPixels(screenHeight)
+            );
+
+            Vector2 position = new Vector2(
+                ComputedStyle.Left.Value.GetPixels(screenWidth),
+                ComputedStyle.Top.Value.GetPixels(screenHeight)
+            );
+
+            if (MousePosition.x > -DRAG_OBLIGINGNESS && MousePosition.x < DRAG_OBLIGINGNESS)
+            {
+                CurrentDragAnchor = DragAnchor.LEFT;
+            }
+            else
+            {
+                CurrentDragAnchor = null;
             }
         }
     }
