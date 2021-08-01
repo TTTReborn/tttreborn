b6b5d0ff8a44124f43fb4485538a0fee0922d086
Matthew Zegar <matthewzegar@gmail.com>
2021-08-01 14:35:41 -0700

    Use hitboxes when looking at entities

--- code/player/TTTPlayer.Lookup.cs
+++ code/player/TTTPlayer.Lookup.cs
@@ -4,10 +4,6 @@ namespace TTTReborn.Player
 {
     partial class TTTPlayer
     {
-        public TTTPlayer Player { get; set; }
-
-        private const float MAX_DRAW_DISTANCE = 500;
-
         public T IsLookingAtType<T>(float distance)
         {
             Trace trace;
@@ -23,27 +19,21 @@ namespace TTTReborn.Player
                 trace = Trace.Ray(EyePos, EyePos + EyeRot.Forward * distance);
             }
 
-            trace = trace.HitLayer(CollisionLayer.Debris)
-                .Ignore(this);
+            trace = trace.HitLayer(CollisionLayer.Debris).Ignore(this);
 
             if (IsSpectatingPlayer)
             {
                 trace.Ignore(CurrentPlayer);
             }
 
-            TraceResult tr = trace.Run();
+            TraceResult tr = trace.UseHitboxes().Run();
 
             if (tr.Hit && tr.Entity is T type)
             {
                 return type;
             }
 
-            return default(T);
-        }
-
-        private TTTPlayer IsLookingAtPlayer()
-        {
-            return IsLookingAtType<TTTPlayer>(MAX_DRAW_DISTANCE);
+            return default;
         }
     }
 }
