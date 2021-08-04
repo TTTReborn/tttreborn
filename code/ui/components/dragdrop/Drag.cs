e4e223e1643a3013ffed0ce689226ab65049237b
Alf21 <alf21@neoxult.de>
2021-08-04 18:08:09 +0200

    UI Components: Added switch component (#174)
    * Added switch component
    
    * Added cursor
    
    * Added DragDrop
    
    * Implemented more DragDrop mechanics
    
    * Added Drop
    
    * Added functional drag and drop

---
+++ code/ui/components/dragdrop/Drag.cs
@@ -0,0 +1,239 @@
+using System.Numerics;
+
+using Sandbox;
+using Sandbox.UI;
+
+// TODO use M4x4 transform
+
+namespace TTTReborn.UI
+{
+    public partial class Drag : DragDrop
+    {
+        public bool IsDragging
+        {
+            get => _isDragging;
+            private set
+            {
+                _isDragging = value;
+
+                if (value == false)
+                {
+                    DragBasePanel.Style.Position = _oldPositionMode;
+
+                    _oldPositionMode = null;
+                }
+                else
+                {
+                    if (_oldPositionMode == null)
+                    {
+                        _oldPositionMode = DragBasePanel.Style.Position;
+                    }
+
+                    DragBasePanel.Style.Position = PositionMode.Absolute;
+                }
+            }
+        }
+        private bool _isDragging = false;
+
+        public bool IsFreeDraggable
+        {
+            get => _isFreeDraggable;
+            set
+            {
+                _isFreeDraggable = value;
+
+                DragBasePanel.Style.Position = PositionMode.Absolute;
+                DragBasePanel.Style.Dirty();
+            }
+        }
+        private bool _isFreeDraggable = false;
+
+        public Panel DragBasePanel
+        {
+            get => _dragBasePanel ?? this;
+            set
+            {
+                _dragBasePanel = value;
+            }
+        }
+        private Panel _dragBasePanel;
+
+        private Vector2 _draggingMouseStartPosition;
+        private Vector2 _draggingStartPosition;
+        private Length? _oldPositionLeft;
+        private Length? _oldPositionTop;
+        private PositionMode? _oldPositionMode;
+
+        public Drag(Panel parent = null) : base(parent)
+        {
+            Parent = parent ?? Parent;
+
+            StyleSheet.Load("/ui/components/dragdrop/DragDrop.scss");
+        }
+
+        protected override void OnMouseDown(MousePanelEvent e)
+        {
+            if (!IsVisible)
+            {
+                return;
+            }
+
+            _draggingMouseStartPosition = Mouse.Position;
+            _draggingStartPosition = new Vector2(DragBasePanel.ComputedStyle.Left?.GetPixels(Screen.Width) ?? 0f, DragBasePanel.ComputedStyle.Top?.GetPixels(Screen.Height) ?? 0f);
+            _oldPositionLeft = DragBasePanel.Style.Left;
+            _oldPositionTop = DragBasePanel.Style.Top;
+
+            // TODO move child to latest child (high zindex)
+
+            IsDragging = true;
+        }
+
+        protected override void OnMouseUp(MousePanelEvent e)
+        {
+            if (IsDragging)
+            {
+                bool noDropTarget = string.IsNullOrEmpty(DragDropGroupName);
+
+                Drop TargetDrop = noDropTarget ? null : GetDropPanel(); // TODO get the right hovering position
+
+                if (noDropTarget || TargetDrop != null && DragDropGroupName == TargetDrop.DragDropGroupName)
+                {
+                    OnDragPanelSuccess(TargetDrop);
+                }
+                else
+                {
+                    OnDragPanelFailed(TargetDrop);
+                }
+
+                OnDragPanelFinished();
+
+                IsDragging = false;
+            }
+        }
+
+        protected override void OnMouseMove(MousePanelEvent e)
+        {
+            if (!IsDragging)
+            {
+                return;
+            }
+
+            Vector2 position = new Vector2(
+                (Mouse.Position.x - _draggingMouseStartPosition.x) + _draggingStartPosition.x,
+                (Mouse.Position.y - _draggingMouseStartPosition.y) + _draggingStartPosition.y
+            );
+
+            float screenWidth = Screen.Width;
+            float screenHeight = Screen.Height;
+
+            float parentWidth = DragBasePanel.Box.Rect.width;
+            float parentHeight = DragBasePanel.Box.Rect.height;
+
+            float left = position.x;
+            float top = position.y;
+
+            Matrix? matrix = DragBasePanel.GlobalMatrix;
+
+            if (matrix != null)
+            {
+                Matrix4x4 matrix4X4 = matrix.Value.Numerics;
+
+                position.x = position.x - matrix4X4.M41;
+                position.y = position.y - matrix4X4.M42;
+            }
+
+            if (IsFreeDraggable)
+            {
+                if (position.x < 0)
+                {
+                    left = 0f;
+
+                    if (matrix != null)
+                    {
+                        left += matrix.Value.Numerics.M41;
+                    }
+                }
+                else if (position.x + parentWidth > screenWidth)
+                {
+                    left = screenWidth - parentWidth;
+
+                    if (matrix != null)
+                    {
+                        left += matrix.Value.Numerics.M41;
+                    }
+                }
+
+                if (position.y < 0f)
+                {
+                    top = 0f;
+
+                    if (matrix != null)
+                    {
+                        top += matrix.Value.Numerics.M42;
+                    }
+                }
+                else if (position.y + parentHeight > screenHeight)
+                {
+                    top = screenHeight - parentHeight;
+
+                    if (matrix != null)
+                    {
+                        top += matrix.Value.Numerics.M42;
+                    }
+                }
+            }
+
+            OnDragPanel(left, top);
+        }
+
+        public virtual void OnDragPanel(float left, float top)
+        {
+            DragBasePanel.Style.Left = Length.Pixels(left);
+            DragBasePanel.Style.Top = Length.Pixels(top);
+
+            DragBasePanel.Style.Dirty();
+        }
+
+        public virtual void OnDragPanelFinished()
+        {
+
+        }
+
+        public virtual void OnDragPanelSuccess(Drop targetDrop, int? index = null)
+        {
+            if (targetDrop != null)
+            {
+                targetDrop.AddChild(this, index ?? targetDrop.ChildCount - 1);
+
+                if (!IsFreeDraggable)
+                {
+                    DragBasePanel.Style.Left = null;
+                    DragBasePanel.Style.Top = null;
+
+                    DragBasePanel.Style.Dirty();
+                }
+            }
+        }
+
+        public virtual void OnDragPanelFailed(Drop targetDrop)
+        {
+            DragBasePanel.Style.Left = _oldPositionLeft;
+            DragBasePanel.Style.Top = _oldPositionTop;
+
+            DragBasePanel.Style.Dirty();
+        }
+
+        private Drop GetDropPanel()
+        {
+            foreach (Drop drop in Drop.List)
+            {
+                if (drop.IsInside(Mouse.Position))
+                {
+                    return drop;
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- code/ui/generalhud/menu/MenuHeader.cs
+++ code/ui/generalhud/menu/MenuHeader.cs
@@ -3,32 +3,66 @@ using Sandbox.UI.Construct;
 
 namespace TTTReborn.UI.Menu
 {
-    public partial class MenuHeader : DraggableHeader
+    public partial class MenuHeader : TTTPanel
     {
-        public Button HomeButton { get; private set; }
-        public Button PreviousButton { get; private set; }
-        public Button NextButton { get; private set; }
+        public readonly MenuNavigationHeader NavigationHeader;
+
+        private MenuDragHeader _dragHeaderWrapper;
 
         public MenuHeader(Menu parent) : base(parent)
         {
             Parent = parent;
 
             StyleSheet.Load("/ui/generalhud/menu/MenuHeader.scss");
+
+            _dragHeaderWrapper = new MenuDragHeader(this, Parent as Menu);
+            NavigationHeader = new MenuNavigationHeader(_dragHeaderWrapper, Parent as Menu);
+        }
+    }
+
+    public partial class MenuNavigationHeader : PanelHeader
+    {
+        public Button HomeButton { get; private set; }
+        public Button PreviousButton { get; private set; }
+        public Button NextButton { get; private set; }
+
+        public readonly Menu Menu;
+
+        public MenuNavigationHeader(Panel parent, Menu menu) : base(parent)
+        {
+            Parent = parent;
+            Menu = menu;
+
+            OnClose = (panelHeader) =>
+            {
+                Menu.IsShowing = false;
+            };
         }
 
         public override void OnCreateHeader()
         {
-            HomeButton = new Button("home", "", () => (Parent as Menu).OpenHomepage());
+            HomeButton = new Button("home", "", () => Menu.OpenHomepage());
             HomeButton.AddClass("home");
 
             AddChild(HomeButton);
 
-            PreviousButton = Add.Button("<", "previous", () => (Parent as Menu).MenuContent.Previous());
-            NextButton = Add.Button(">", "next", () => (Parent as Menu).MenuContent.Next());
+            PreviousButton = Add.Button("<", "previous", () => Menu.MenuContent.Previous());
+            NextButton = Add.Button(">", "next", () => Menu.MenuContent.Next());
 
             HomeButton.SetClass("disabled", true);
             PreviousButton.SetClass("disabled", true);
             NextButton.SetClass("disabled", true);
+        }
+    }
+
+    public partial class MenuDragHeader : Drag
+    {
+        public MenuDragHeader(Panel parent, Menu menu) : base(parent)
+        {
+            Parent = parent;
+            DragBasePanel = menu;
+
+            IsFreeDraggable = true;
         }
     }
 }
