bc9bf6a5ec4cc7789dcd60299e67c74d1244ea80
pBinarySoul <pBinarySoul@gmail.com>
2021-08-05 17:07:34 +0300

    Now languages can be loaded from JSON file If GetLanguageByName tries to get language that doesn't exist, it falls back to English

--- code/lang/ILanguage.cs
+++ code/lang/ILanguage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using Sandbox;
 
@@ -10,28 +11,47 @@ namespace TTTReborn.Language
         [ClientVar("ttt_language", Help = "Your current language", Saved = true)]
         public static string ActiveLanguage { get; set; }
 
-        [ServerVar("ttt_server_language", Help = "Server's current language")]
+        [ServerVar("ttt_server_language", Help = "Server's current language", Saved = true)]
         public static string ServerLanguage { get; set; }
         public const string FALLBACK_LANGUAGE = "English";
 
         public static Dictionary<string, TLanguage> Languages = new();
 
-        public static void LoadLanguages()
+        private static void FallbackLanguage()
         {
-            // Forgive me, father, for I have sinned
-            Languages.Add("English", new TLanguage("English", "{ \"LanguageName\": \"English\", \"RoleName_Innocent\": \"Innocent\", \"RoleName_Traitor\": \"Traitor\", \"RoundState_Waiting\": \"Waiting\", \"RoundState_Preparing\": \"Preparing\", \"RoundState_In Progress\": \"In progress\", \"RoundState_Post\": \"Post round\", \"TeamName_Innocents\": \"Innocents\", \"TeamName_Traitors\": \"Traitors\", \"PostRound_Header\": \"{0} WIN!\", \"PostRound_Text\": \"Thanks for playing TTT Reborn, more updates and stats to come!\"}"));
-            Languages.Add("Russian", new TLanguage("Russian", "{ \"LanguageName\": \"Русский\", \"RoleName_Innocent\": \"Невиновный\", \"RoleName_Traitor\": \"Предатель\", \"RoundState_Waiting\": \"Ожидание\", \"RoundState_Preparing\": \"Подготовка\", \"RoundState_In Progress\": \"В процессе\", \"RoundState_Post\": \"Конец\", \"TeamName_Innocents\": \"Невиновные\", \"TeamName_Traitors\": \"Предатели\", \"PostRound_Header\": \"{0} ВЫЙГРАЛИ!\", \"PostRound_Text\": \"Спасибо, что играете в TTT Reborn!\" }"));
-            Languages.Add("French", new TLanguage("French", "{ \"LanguageName\": \"Français\", \"RoleName_Innocent\": \"Innocent\", \"RoleName_Traitor\": \"Traitre\", \"RoundState_Waiting\": \"En attente\", \"RoundState_Preparing\": \"Préparation\", \"RoundState_In Progress\": \"En cours\", \"RoundState_Post\": \"Terminé\", \"TeamName_Innocents\": \"INNOCENTS\", \"TeamName_Traitors\": \"TRAITRES\", \"PostRound_Header\": \"VICTOIRE DES {0}!\", \"PostRound_Text\": \"Merci d'avoir joué à TTT Reborn!\" }"));
+            //Log.Info($"Falling back to {FALLBACK_LANGUAGE}.");
 
-            if ( ActiveLanguage is null )
+            if (Host.IsClient)
                 ActiveLanguage = FALLBACK_LANGUAGE;
+
+            if (Host.IsServer)
+                ConsoleSystem.SetValue("ttt_server_language", FALLBACK_LANGUAGE);
+        }
+        public static void LoadLanguages()
+        {            
+            foreach ( string file in FileSystem.Mounted.FindFile("/lang/packs/", "*.json", false) )
+            {
+                string name = Path.GetFileNameWithoutExtension(file);
+                string json = FileSystem.Mounted.ReadAllText($"/lang/packs/{file}");
+                Languages.Add(name, new TLanguage(name, json));
+
+                Log.Info($"Added TTT language pack: {name}.");
+            }
+
+            FallbackLanguage();
         }
 
         public static TLanguage GetActiveLanguage() => GetLanguageByName( Host.IsServer ? ServerLanguage : ActiveLanguage );
         public static TLanguage GetLanguageByName(string name)
         {
             TLanguage lang = null;
-            Languages.TryGetValue(name, out lang);
+            if (!Languages.TryGetValue(name, out lang))
+            {
+                Log.Info($"Tried to get a language that doesn't exist: {name}.");
+
+                if (Host.IsServer ? ServerLanguage == name : ActiveLanguage == name)
+                    FallbackLanguage();
+            }
             return lang;
         }
     }
