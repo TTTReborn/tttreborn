2d892f296cedf4aab725c48c52b52b11eb1e238f
Krzyśka Filip <45741043+Krzyska-Filip@users.noreply.github.com>
2021-09-02 01:44:56 +0200

    Fix for #204 (Damage Overlay bugged) (#214)
    * Update DamageIndicator.cs
    
    Fixed issue where maximum duration
    of indicator was multiplied instead of getting max value out of it.
    
    * Revert "Update DamageIndicator.cs"
    
    This reverts commit 65318f231fd07d982d4e6f021db6628d2ea1fa1c.
    
    * Fix for stacking indicators.
    
    AliveHud no longer stacks
    
    * Fix for damage triggers
    
    I don't know why but it fixes the problem so I won't complain.
    
    * Update DamageIndicator.cs
    
    Code cleaning
    
    * Update DamageIndicator.cs
    
    Code cleaning
    
    * Removed unnecessary namespaces in Hud.cs

--- code/ui/Hud.cs
+++ code/ui/Hud.cs
@@ -33,34 +33,7 @@ namespace TTTReborn.UI
                 Local.Hud?.Delete();
 
                 Hud hud = new();
-
-                if (Local.Client.Pawn is TTTPlayer player && player.LifeState == LifeState.Alive)
-                {
-                    hud.AliveHudPanel.CreateHud();
-                }
             }
-        }
-
-        [Event("tttreborn.player.spawned")]
-        private void OnPlayerSpawned(TTTPlayer player)
-        {
-            if (player != Local.Client.Pawn)
-            {
-                return;
-            }
-
-            Current?.AliveHudPanel.CreateHud();
-        }
-
-        [Event("tttreborn.player.died")]
-        private void OnPlayerDied(TTTPlayer player)
-        {
-            if (player != Local.Client.Pawn)
-            {
-                return;
-            }
-
-            Current?.AliveHudPanel.DeleteHud();
         }
 
         public class GeneralHud : TTTPanel
@@ -87,18 +60,10 @@ namespace TTTReborn.UI
             public AliveHud(Panel parent)
             {
                 Parent = parent;
-            }
 
-            public void CreateHud()
-            {
-                Parent.AddChild<DamageIndicator>();
-                Parent.AddChild<QuickShop>();
                 Parent.AddChild<DrowningIndicator>();
-            }
-
-            public void DeleteHud()
-            {
-                DeleteChildren(true);
+                Parent.AddChild<QuickShop>();
+                Parent.AddChild<DamageIndicator>();
             }
         }
     }
