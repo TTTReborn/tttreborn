27fdd1bbc91ee587198bef7847af9f39ecaf71aa
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2021-09-03 21:31:24 -0400

    Rework QuickShop
    - Add a smooth background popup overlay
    - Rework code structure for the QuickShop implementation
    - Rework behavior for selecting items in QuickShop

--- code/ui/alivehud/quickshop/QuickShop.cs
+++ code/ui/alivehud/quickshop/QuickShop.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+
 using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
 
 using TTTReborn.Items;
 using TTTReborn.Player;
@@ -9,30 +14,107 @@ namespace TTTReborn.UI
     {
         public static QuickShop Instance;
 
-        private static ShopItemData? _selectedItemData;
+        public static ShopItemData? _selectedItemData;
 
-        private Header _header;
-        private Footer _footer;
-        private readonly Content _content;
+        private readonly List<QuickShopItem> _items = new();
+        private Label _creditLabel;
+        private Panel _itemPanel;
+        private Label _itemDescriptionLabel;
 
         private int _credits = 0;
 
-        public QuickShop()
+        public QuickShop() : base()
         {
             Instance = this;
 
             StyleSheet.Load("/ui/alivehud/quickshop/QuickShop.scss");
 
-            _header = new(this);
-            _content = new(this);
-            _footer = new(this);
+            AddClass("background-color-secondary");
+            AddClass("text-shadow");
+
+            _creditLabel = Add.Label();
+            _creditLabel.AddClass("credit-label");
+
+            _itemPanel = new Panel(this);
+            _itemPanel.AddClass("item-panel");
+            _itemPanel.AddClass("rounded");
+
+            _itemDescriptionLabel = Add.Label();
+            _itemDescriptionLabel.AddClass("item-description-label");
+
+            PopulateItems();
 
             Enabled = false;
         }
 
+        public void PopulateItems()
+        {
+            foreach (Type type in Globals.Utils.GetTypes<IBuyableItem>())
+            {
+                IBuyableItem item = Globals.Utils.GetObjectByType<IBuyableItem>(type);
+                ShopItemData itemData = item.CreateItemData();
+
+                item.Delete();
+
+                if (_selectedItemData == null)
+                {
+                    _selectedItemData = itemData;
+                }
+
+                AddItem(itemData);
+            }
+        }
+
+        private void AddItem(ShopItemData itemData)
+        {
+            QuickShopItem item = new(_itemPanel);
+            item.SetItem(itemData);
+
+            item.AddEventListener("onmouseover", () =>
+            {
+                _selectedItemData = itemData;
+
+                Update();
+            });
+
+            item.AddEventListener("onmouseout", () =>
+            {
+                _selectedItemData = null;
+
+                Update();
+            });
+
+            item.AddEventListener("onclick", () =>
+            {
+                if (item.IsDisabled)
+                {
+                    return;
+                }
+
+                if (_selectedItemData?.IsBuyable(Local.Pawn as TTTPlayer) ?? false)
+                {
+                    ConsoleSystem.Run("ttt_requestitem", item.ItemData?.Name);
+                }
+
+                Update();
+            });
+
+            _items.Add(item);
+        }
+
         public void Update()
         {
-            _content.Update();
+            _creditLabel.Text = $"You have ${_credits}";
+
+            foreach (QuickShopItem item in _items)
+            {
+                item.Update();
+            }
+
+            _itemDescriptionLabel.Text = _selectedItemData != null ?
+                $"The description for {_selectedItemData?.Name} will go here" : "";
+
+            SetClass("opacity-90", Enabled);
         }
 
         public override void Tick()
@@ -44,11 +126,11 @@ namespace TTTReborn.UI
                 return;
             }
 
-            int credits = (Local.Pawn as TTTPlayer).Credits;
+            int newCredits = (Local.Pawn as TTTPlayer).Credits;
 
-            if (_credits != credits)
+            if (_credits != newCredits)
             {
-                _credits = credits;
+                _credits = newCredits;
 
                 Update();
             }
@@ -75,9 +157,8 @@ namespace TTTReborn.Player
                 return;
             }
 
-            QuickShop.Instance.Update();
-
             QuickShop.Instance.Enabled = true;
+            QuickShop.Instance.Update();
         }
 
         [ClientCmd("-ttt_quickshop")]
@@ -89,6 +170,7 @@ namespace TTTReborn.Player
             }
 
             QuickShop.Instance.Enabled = false;
+            QuickShop.Instance.Update();
         }
     }
 }
---
+++ code/ui/alivehud/quickshop/QuickShopItem.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+using TTTReborn.Items;
+using TTTReborn.Player;
+
+namespace TTTReborn.UI
+{
+    public partial class QuickShopItem : Panel
+    {
+        public ShopItemData? ItemData;
+
+        private Panel _itemIcon;
+        private Label _itemNameLabel;
+        private Label _itemPriceLabel;
+
+        public bool IsDisabled = false;
+
+        public QuickShopItem(Sandbox.UI.Panel parent) : base(parent)
+        {
+            Parent = parent;
+
+            AddClass("rounded");
+            AddClass("text-shadow");
+            AddClass("background-color-secondary");
+
+            _itemNameLabel = Add.Label();
+            _itemNameLabel.AddClass("item-name-label");
+
+            _itemIcon = new Panel(this);
+            _itemIcon.AddClass("item-icon");
+
+            _itemPriceLabel = Add.Label();
+            _itemPriceLabel.AddClass("item-price-label");
+        }
+
+        public void SetItem(ShopItemData buyableItemData)
+        {
+            ItemData = buyableItemData;
+
+            _itemNameLabel.Text = $"{buyableItemData.Name}";
+            _itemPriceLabel.Text = $"${buyableItemData.Price}";
+
+            _itemIcon.Style.Background = new PanelBackground
+            {
+                Texture = Texture.Load($"/ui/weapons/{buyableItemData.Name}.png")
+            };
+
+            _itemIcon.Style.Dirty();
+        }
+
+        public void Update()
+        {
+            IsDisabled = (Local.Pawn as TTTPlayer).CanBuy(ItemData) != BuyError.None;
+            Enabled = !IsDisabled;
+        }
+    }
+}
