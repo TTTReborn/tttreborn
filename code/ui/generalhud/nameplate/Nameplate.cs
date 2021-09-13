39ad358a4003b2b3b735224e266e4dcafe97ab68
Alexander Mastryukov <12959805+AMastryukov@users.noreply.github.com>
2021-09-13 18:15:59 -0400

    Update code/ui/generalhud/nameplate/Nameplate.cs
    Co-authored-by: Alf21 <alf21@neoxult.de>

--- code/ui/generalhud/nameplate/Nameplate.cs
+++ code/ui/generalhud/nameplate/Nameplate.cs
@@ -66,7 +66,7 @@ namespace TTTReborn.UI
                 }
             }
 
-            return HealthGroupList[HealthGroupList.Length - 1];
+            return HealthGroupList[^1];
         }
 
         public override void UpdateHintPanel()
