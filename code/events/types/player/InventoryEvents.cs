dee1032b460705c5854dfd307f992ac9561e3319
Larry <gravitywolfnotamused@gmail.com>
2021-09-28 15:40:03 -0600

    Create events for Inventory

---
+++ code/events/types/player/InventoryEvents.cs
@@ -0,0 +1,20 @@
+namespace TTTReborn.Events
+{
+    public class InventoryEvents
+    {
+        /// <summary>
+        /// Triggered when a item is picked up. <c>ICarriableItem</c> of the object that is picked up is passed to events.
+        /// </summary>
+        public TTTEvents OnItemPickUp => new("tttreborn.player.inventory.pickup");
+
+        /// <summary>
+        /// Triggered when a item is picked up. <c>ICarriableItem</c> of the object that is dropped is passed to events.
+        /// </summary>
+        public TTTEvents OnItemDropped => new("tttreborn.player.inventory.drop");
+
+        /// <summary>
+        /// Triggered when the player's inventory is cleared. <strong>No</strong> data is passed to this event.
+        /// </summary>
+        public TTTEvents ClearInventory => new("tttreborn.player.inventory.clear");
+    }
+}
