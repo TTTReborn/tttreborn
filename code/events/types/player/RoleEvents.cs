0f27e649ef7777c2fccc43a2a9f55585e341f87d
Larry <gravitywolfnotamused@gmail.com>
2021-09-28 15:52:54 -0600

    Changed documentation.

--- code/events/types/player/RoleEvents.cs
+++ code/events/types/player/RoleEvents.cs
@@ -3,7 +3,7 @@ namespace TTTReborn.Events
     public class RoleEvents
     {
         /// <summary>
-        /// Triggered when the player's inventory is cleared. <strong>No</strong> data is passed to this event.
+        /// Triggered when the player's inventory is cleared. <strong>TTTPlayer</strong> is passed to this event.
         /// </summary>
         public TTTEvents OnSelect => new("tttreborn.player.role.onselect");
     }
