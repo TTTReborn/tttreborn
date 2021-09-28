17747e64b37528c36331f41e1fbdf63d973b1c72
Larry <gravitywolfnotamused@gmail.com>
2021-09-28 15:52:39 -0600

    Reordered events, made role events player events instead of own class.

--- code/events/types/player/PlayerEvents.cs
+++ code/events/types/player/PlayerEvents.cs
@@ -3,23 +3,23 @@ namespace TTTReborn.Events
     public class PlayerEvents
     {
         /// <summary>
-        /// Triggered when a player dies. <c>TTTPlayer</c> object is passed to events.
-        /// </summary>
-        public TTTEvents Died => new("tttreborn.player.died");
-
-        /// <summary>
         /// Triggered when a player spawns. <c>TTTPlayer</c> object is passed to events.
         /// </summary>
         public TTTEvents Spawned => new("tttreborn.player.spawned");
 
         /// <summary>
-        /// Events that are specifically triggered with role changes.
+        /// Triggered when a player dies. <c>TTTPlayer</c> object is passed to events.
         /// </summary>
-        public RoleEvents RoleEvents { get; } = new();
+        public TTTEvents Died => new("tttreborn.player.died");
+
+        /// <summary>
+        /// Triggered when the player's inventory is cleared. <strong>TTTPlayer</strong> is passed to this event.
+        /// </summary>
+        public TTTEvents OnRoleSelected => new("tttreborn.player.role.onselect");
 
         /// <summary>
         /// Events that are specifically triggered with inventory changes.
         /// </summary>
-        public InventoryEvents InventoryEvents { get; } = new();
+        public InventoryEvents Inventory { get; } = new();
     }
 }
