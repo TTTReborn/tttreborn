0046a96eb046d8f9068d981354b90a679bd3427b
Alf Müller <alf21@neoxult.de>
2021-10-21 15:42:01 +0200

    Fixed Style.Background regarding latest sbox breaking changes

--- code/ui/alivehud/quickshop/QuickShopItem.cs
+++ code/ui/alivehud/quickshop/QuickShopItem.cs
@@ -40,14 +40,7 @@ namespace TTTReborn.UI
             _itemNameLabel.Text = $"{shopItemData.Name}";
             _itemPriceLabel.Text = $"${shopItemData.Price}";
 
-            Texture icon = Texture.Load($"/ui/weapons/{shopItemData.Name}.png", false);
-            icon ??= Texture.Load($"/ui/none.png");
-
-            _itemIcon.Style.Background = new PanelBackground
-            {
-                Texture = icon
-            };
-
+            _itemIcon.Style.BackgroundImage = Texture.Load($"/ui/weapons/{shopItemData.Name}.png", false) ?? Texture.Load($"/ui/none.png");
             _itemIcon.Style.Dirty();
         }
 
