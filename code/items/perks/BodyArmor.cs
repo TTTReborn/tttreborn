7183672fe2ae8a9c5dea35e033c319ff9e3b1d07
Alf Müller <alf21@neoxult.de>
2021-10-30 01:27:49 +0200

    Every item costs 100 bucks by default

--- code/items/perks/BodyArmor.cs
+++ code/items/perks/BodyArmor.cs
@@ -3,7 +3,7 @@ using Sandbox;
 namespace TTTReborn.Items
 {
     [Library("perk_bodyarmor")]
-    [Buyable(Price = 0)]
+    [Buyable(Price = 100)]
     [Hammer.Skip]
     public partial class BodyArmor : TTTPerk
     {
