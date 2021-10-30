0e6d4e469eea272cfc270dcbf838749b25c0c5a7
Alf Müller <alf21@neoxult.de>
2021-10-31 01:30:46 +0200

    Implemented #256

--- code/items/weapons/TTTWeaponRandom.cs
+++ code/items/weapons/TTTWeaponRandom.cs
@@ -10,6 +10,14 @@ namespace TTTReborn.Items
     [Library("entity_weapon_random")]
     public class TTTWeaponRandom : Entity
     {
+        private static readonly int AMMO_DISTANCE_UP = 24;
+
+        /// <summary>
+        /// Defines the amount of matching ammo entities that should be spawned near the weapons.
+        /// </summary>
+        [Property(Title = "Amount of Ammo")]
+        public int AmmoToSpawn { get; set; } = 0;
+
         public void Activate()
         {
             List<Type> wepTypes = Globals.Utils.GetTypesWithAttribute<TTTWeapon, SpawnableAttribute>();
@@ -19,11 +27,30 @@ namespace TTTReborn.Items
                 return;
             }
 
-            Type typeToSpawn = wepTypes[Utils.RNG.Next(wepTypes.Count)];
-            TTTWeapon ent = Globals.Utils.GetObjectByType<TTTWeapon>(typeToSpawn);
-            ent.Position = Position;
-            ent.Rotation = Rotation;
-            ent.Spawn();
+            Type weaponTypeToSpawn = Utils.RNG.FromList(wepTypes);
+            TTTWeapon weapon = Globals.Utils.GetObjectByType<TTTWeapon>(weaponTypeToSpawn);
+            weapon.Position = Position;
+            weapon.Rotation = Rotation;
+            weapon.Spawn();
+
+            if (weapon.AmmoEntity == null)
+            {
+                return; // If the choosen weapon doesn't use ammo we don't need to spawn any.
+            }
+
+            if (!weapon.AmmoEntity.IsSubclassOf(typeof(TTTAmmo)))
+            {
+                Log.Error($"The defined ammo type {weapon.AmmoEntity.Name} for the weapon {weapon.LibraryName} is not a descendant of {typeof(TTTAmmo).Name}.");
+
+                return;
+            }
+
+            for (int i = 0; i < AmmoToSpawn; i++)
+            {
+                TTTAmmo ammo = Utils.GetObjectByType<TTTAmmo>(weapon.AmmoEntity);
+                ammo.Position = weapon.Position + Vector3.Up * AMMO_DISTANCE_UP;
+                ammo.Spawn();
+            }
         }
     }
 }
